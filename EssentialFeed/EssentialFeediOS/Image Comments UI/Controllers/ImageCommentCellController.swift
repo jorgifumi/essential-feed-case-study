02fe667b6e8b51f211f57e8c52fef5429e29f6c5
Jorge Lucena <jorgifumi@gmail.com>
2023-03-13 11:06:05 +0100

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -0,0 +1,33 @@
+//
+//  ImageCommentCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Jorge Lucena on 9/3/23.
+//
+
+import UIKit
+import EssentialFeed
+
+public final class ImageCommentCellController: CellController {
+    private let model: ImageCommentViewModel
+
+    public init(model: ImageCommentViewModel) {
+        self.model = model
+    }
+
+    public func view(in tableView: UITableView) -> UITableViewCell {
+        let cell: ImageCommentCell = tableView.dequeueReusableCell()
+        cell.messageLabel.text = model.message
+        cell.userNameLabel.text = model.userName
+        cell.dateLabel.text = model.date
+        return cell
+    }
+
+    public func preload() {
+
+    }
+
+    public func cancelLoad() {
+
+    }
+}
---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Jorge Lucena on 10/3/23.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var userNameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+}
